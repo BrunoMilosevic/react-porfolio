ee384ef829a20d24966462c59bd98f7d6fca2868
BrunoMilosevic <brunomilosevic1234@gmail.com>
2024-02-04 19:08:04 +0100

    added map

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,37 @@
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
-
+  const form = useRef();
   useEffect(() => {
     setTimeout(() => {
       setLetterClass("text-animate-hover");
     }, 4000);
   }, []);
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_25ymqoq",
+        "template_s5hj1eb",
+        form.current,
+        "zhi0k43gm9PYC0b-9"
+      )
+      .then(
+        () => {
+          alert("Message successfully sent!");
+          window.location.reload(false);
+        },
+        () => {
+          alert("Failed to send the message! Please try again");
+        }
+      );
+  };
   return (
     <>
       <div className="container contact-page">
@@ -26,7 +48,7 @@ const Contact = () => {
             other requests or any questions feel free to contact me!
           </p>
           <div className="contact-form">
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
               <ul>
                 <li className="half">
                   <input type="text" name="name" placeholder="Name" required />
@@ -53,13 +75,28 @@ const Contact = () => {
                     name="message"
                     required
                   ></textarea>
+                  <li>
+                    <input type="submit" className="flat-button" value="SEND" />
+                  </li>
                 </li>
               </ul>
-              <li>
-                <input type="submit" className="flat-button" value="SEND" />
-              </li>
             </form>
           </div>
+        </div>
+        <div className="info-map">
+          Bruno Milošević,
+          <br />
+          Osijek Croatia
+          <br />
+          <span>brunomilosevic1234@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[45.498647, 18.709717]} zoom={5}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[45.498647, 18.709717]}>
+              <Popup>Bruno</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
     </>
