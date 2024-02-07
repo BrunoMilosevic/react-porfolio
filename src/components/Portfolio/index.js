08c7b0ee315a34b7a052aabec807afa26da28c2a
BrunoMilosevic <brunomilosevic1234@gmail.com>
2024-02-07 12:38:27 +0100

    Portfolio page layout created

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
-
+import AnimatedLetters from "../AnimatedLetters";
 const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
   return (
     <div className="container portfolio-page">
-      <h1 className="page-title">Portfolio</h1>
+      <h1 className="page-title">
+        <AnimatedLetters
+          idx={15}
+          strArray={"PORTFOLIO".split("")}
+          letterClass={letterClass}
+        />
+      </h1>
+      <div>{renderPorfolio()}</div>
     </div>
   );
 };
